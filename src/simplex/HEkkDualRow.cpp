5e1e0693109729ffce2dd9499a267fe7ea0763c4
Julian Hall <jajhall@ed.ac.uk>
2022-04-01 21:53:45 +0100

    Formatted

--- src/simplex/HEkkDualRow.cpp
+++ src/simplex/HEkkDualRow.cpp
@@ -172,7 +172,7 @@ HighsInt HEkkDualRow::chooseFinal() {
     analysis->num_quad_chuzc++;
     analysis->sum_quad_chuzc_size += workCount;
     analysis->max_quad_chuzc_size =
-      max(workCount, analysis->max_quad_chuzc_size);
+        max(workCount, analysis->max_quad_chuzc_size);
   } else {
     analysis->num_heap_chuzc++;
     analysis->sum_heap_chuzc_size += workCount;
