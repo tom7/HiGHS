dc2670a675561602fe7838de1a04a8ac8535a677
Ivet Galabova <galabovaa@gmail.com>
2023-08-07 14:40:28 +0300

    versions meson, pyproject, pytest

--- highspy/tests/test_highspy.py
+++ highspy/tests/test_highspy.py
@@ -76,7 +76,7 @@ class TestHighsPy(unittest.TestCase):
     
     def test_version(self):
         h = self.get_basic_model()
-        self.assertEqual(h.version(), "1.5.3")
+        self.assertEqual(h.version(), "1.5.4")
         self.assertEqual(h.versionMajor(), 1)
         self.assertEqual(h.versionMinor(), 5)
         self.assertEqual(h.versionPatch(), 3)
